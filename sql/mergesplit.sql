5a076ca92bc044ae642b199e48701324705b7671
Haim Hong <4951hh@gmail.com>
2022-10-27 13:28:18 -0400

    update to sql

--- sql/mergesplit.sql
+++ sql/mergesplit.sql
@@ -24,10 +24,40 @@ CREATE TABLE GameInstance (
 )
 
 CREATE TABLE PlayerScores (
-  ID integer PRIMARY KEY
+  playerID integer REFERENCES Player(ID)
   gameInstanceID1 REFERENCES GameInstance(instanceID)
   gameInstanceID2 REFERENCES GameInstance(instanceID)
   gameInstanceID3 REFERENCES GameInstance(instanceID)
   winStreak integer
   losingStreak integer
 )
+
+GRANT
+SELECT
+  ON Player TO PUBLIC;
+
+GRANT
+SELECT
+  ON GameType TO PUBLIC;
+
+GRANT
+SELECT
+  ON GameInstance TO PUBLIC;
+
+GRANT
+SELECT
+  ON PlayerScores TO PUBLIC;
+
+INSERT INTO Player(ID, email, name) VALUES (1, "39393@gmail.com", "The Legend");
+INSERT INTO Player(ID, email, name) VALUES (1, "45747@gmail.com", "The Second");
+
+INSERT INTO GameType(ID, color, numSquares, difficulty) VALUES (1, "", 50, 3);
+
+INSERT INTO GameInstance(instanceID, gameTypeRefID, gameInstanceID) VALUES (1, 1, 1);
+INSERT INTO GameInstance(instanceID, gameTypeRefID, gameInstanceID) VALUES (2, 1, 1);
+INSERT INTO GameInstance(instanceID, gameTypeRefID, gameInstanceID) VALUES (3, 1, 1);
+
+
+INSERT INTO PlayerScores(ID, gameInstanceID1, gameInstanceID2, gameInstanceID3, winStreak, losingStreak) VALUES (1, 1, , ,1,0); -- left empty for 2 and 3 as only 1 game inserted
+INSERT INTO PlayerScores(ID, gameInstanceID1, gameInstanceID2, gameInstanceID3, winStreak, losingStreak) VALUES (2, 1, , ,1,0);
+INSERT INTO PlayerScores(ID, gameInstanceID1, gameInstanceID2, gameInstanceID3, winStreak, losingStreak) VALUES (3, 1, , ,1,0);
