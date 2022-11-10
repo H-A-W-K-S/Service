cc8eaa29dfc873e80c12181fb839a5d8c7b0a007
Haim Hong <4951hh@gmail.com>
2022-11-09 20:21:20 -0500

    sql update

--- sql/mergesplit.sql
+++ sql/mergesplit.sql
@@ -4,65 +4,52 @@
 
 
 --SQL Data Service for MergeSplit Game
+DROP TABLE IF EXISTS PlayerGame
 DROP TABLE IF EXISTS Player
-DROP TABLE IF EXISTS GameType
-DROP TABLE IF EXISTS GameInstance
-DROP TABLE IF EXISTS PlayerScores
+DROP TABLE IF EXISTS Game
+
 
 CREATE TABLE Player (
-ID integer PRIMARY KEY
-email varchar(50)
-name varchar(50)
+  ID integer PRIMARY KEY,
+  email varchar(50) NOT NULL,
+  name varchar(50)
 )
 
-CREATE TABLE GameType (
-ID integer --Repeat IDs. Only uses a number from one to three.
-color varchar(50)
-numSquares integer
-difficulty integer
+CREATE TABLE Game (
+  ID integer, --Repeat IDs. Only uses a number from one to three.
+  time timestamp
 )
 
-CREATE TABLE GameInstance (
-  instanceID integer REFERENCES Player(ID)
-  gameTypeRefID integer --ID from one to three to reference a specific game version.
-  gameInstanceID integer PRIMARY KEY --Unique identifier for each game.
+CREATE TABLE PlayerGame (
+  gameInstanceID integer REFERENCES Game(ID), --Unique identifier for each game.
+  PlayerID integer REFERENCES Player(ID),
+  score integer
 )
 
-CREATE TABLE PlayerScores (
-  playerID integer REFERENCES Player(ID)
-  gameInstanceID1 REFERENCES GameInstance(instanceID)
-  gameInstanceID2 REFERENCES GameInstance(instanceID)
-  gameInstanceID3 REFERENCES GameInstance(instanceID)
-  winStreak integer
-  losingStreak integer
-)
+GRANT SELECT ON Player TO PUBLIC;
+GRANT SELECT ON Game TO PUBLIC;
+GRANT SELECT ON PlayerGame TO PUBLIC;
 
-GRANT
-SELECT
-  ON Player TO PUBLIC;
+INSERT INTO Player VALUES (1, "39393@gmail.com", "The Legend");
+INSERT INTO Player VALUES (2, "45747@gmail.com", "The Second");
+INSERT INTO Player VALUES (3, "29578@gmail.com", "The Logic");
 
-GRANT
-SELECT
-  ON GameType TO PUBLIC;
+INSERT INTO Game VALUES (1, '2022-07-27 08:00:00');
+INSERT INTO Game VALUES (2, '2022-08-28 13:20:00');
+INSERT INTO Game VALUES (3, '2022-09-29 18:41:00');
+INSERT INTO Game VALUES (4, '2022-10-30 20:55:00');
 
-GRANT
-SELECT
-  ON GameInstance TO PUBLIC;
+INSERT INTO PlayerGame(instanceID, gameTypeRefID, gameInstanceID) VALUES (1, 1, 100);
+INSERT INTO PlayerGame(instanceID, gameTypeRefID, gameInstanceID) VALUES (2, 1, 98);
+INSERT INTO PlayerGame(instanceID, gameTypeRefID, gameInstanceID) VALUES (3, 1, 97);
+INSERT INTO PlayerGame(instanceID, gameTypeRefID, gameInstanceID) VALUES (4, 1, 100);
 
-GRANT
-SELECT
-  ON PlayerScores TO PUBLIC;
+INSERT INTO PlayerGame(instanceID, gameTypeRefID, gameInstanceID) VALUES (1, 2, 100);
+INSERT INTO PlayerGame(instanceID, gameTypeRefID, gameInstanceID) VALUES (2, 2, 98);
+INSERT INTO PlayerGame(instanceID, gameTypeRefID, gameInstanceID) VALUES (3, 2, 97);
+INSERT INTO PlayerGame(instanceID, gameTypeRefID, gameInstanceID) VALUES (4, 2, 100);
 
-INSERT INTO Player(ID, email, name) VALUES (1, "39393@gmail.com", "The Legend");
-INSERT INTO Player(ID, email, name) VALUES (1, "45747@gmail.com", "The Second");
-
-INSERT INTO GameType(ID, color, numSquares, difficulty) VALUES (1, "", 50, 3);
-
-INSERT INTO GameInstance(instanceID, gameTypeRefID, gameInstanceID) VALUES (1, 1, 1);
-INSERT INTO GameInstance(instanceID, gameTypeRefID, gameInstanceID) VALUES (2, 1, 1);
-INSERT INTO GameInstance(instanceID, gameTypeRefID, gameInstanceID) VALUES (3, 1, 1);
-
-
-INSERT INTO PlayerScores(ID, gameInstanceID1, gameInstanceID2, gameInstanceID3, winStreak, losingStreak) VALUES (1, 1, , ,1,0); -- left empty for 2 and 3 as only 1 game inserted
-INSERT INTO PlayerScores(ID, gameInstanceID1, gameInstanceID2, gameInstanceID3, winStreak, losingStreak) VALUES (2, 1, , ,1,0);
-INSERT INTO PlayerScores(ID, gameInstanceID1, gameInstanceID2, gameInstanceID3, winStreak, losingStreak) VALUES (3, 1, , ,1,0);
+INSERT INTO PlayerGame(instanceID, gameTypeRefID, gameInstanceID) VALUES (1, 3, 100);
+INSERT INTO PlayerGame(instanceID, gameTypeRefID, gameInstanceID) VALUES (2, 3, 98);
+INSERT INTO PlayerGame(instanceID, gameTypeRefID, gameInstanceID) VALUES (3, 3, 97);
+INSERT INTO PlayerGame(instanceID, gameTypeRefID, gameInstanceID) VALUES (4, 3, 100);
